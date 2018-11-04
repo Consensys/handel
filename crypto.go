0f5f29c45df19320f1aafbdf10e39b8bfaaa1e57
nikkolasg <nicolas.gailly@consensys.net>
2018-11-04 17:53:49 +0100

    moved all crypto related to crypto.go

--- crypto.go
+++ crypto.go
@@ -28,3 +28,14 @@ type SignatureScheme interface {
 	SecretKey
 	UnmarshalSignature([]byte) (MultiSignature, error)
 }
+
+// MultiSignature holds methods to pass from/to a binary representation and to
+// combine multi-signatures together
+type MultiSignature interface {
+	MarshalBinary() ([]byte, error)
+
+	// Combine "merges" the two signature together so that it produces an unique
+	// multi-signature that can be verified by the combination of both
+	// respective public keys that produced the original signatures.
+	Combine(MultiSignature) MultiSignature
+}
