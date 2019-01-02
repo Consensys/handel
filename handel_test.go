d6c96d4dea9f178bd15a3b18ee154ee962cd1809
nikkolasg <nicolas.gailly@consensys.net>
2019-01-02 18:13:25 +0100

    same tests as dev

--- handel_test.go
+++ handel_test.go
@@ -34,6 +34,8 @@ func TestHandelTestNetworkFull(t *testing.T) {
 		{33, nil, 33, false},
 		{67, off(), 67, false},
 		{5, off(4), 4, false},
+		{13, off(0, 1, 4, 6), 6, false},
+		{128, off(0, 1, 4, 6), 124, false},
 		{10, off(0, 3, 5, 7, 9), 5, false},
 	}
 	testHandelTestNetwork(t, tests)
@@ -49,8 +51,7 @@ func TestHandelTestNetworkLarge(t *testing.T) {
 	off()
 
 	var tests = []handelTest{
-		{223, off(0, 1, 4, 6, 7, 19, 56, 89, 99), 210, false},
-		{128, off(0, 1, 4, 6), 124, false},
+		{333, off(0, 1, 4, 6, 7, 19, 56, 89, 99), 310, false},
 	}
 	testHandelTestNetwork(t, tests)
 }
