486d476f690ac9f790b185297c86dddfbbf3c0bc
Nicolas Liochon <liochon@gmail.com>
2019-01-21 19:54:03 +0100

    fix port allocation for tests

--- simul/lib/net.go
+++ simul/lib/net.go
@@ -8,32 +8,33 @@ import (
 
 
 
-
 // GetFreePort returns a free tcp port or panics
+var afterPort = 10000
 func GetFreePort() int {
-	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	if err != nil {
-		panic(err)
-	}
-
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		l.Close()
+	for i := afterPort + 1; i < afterPort + 1000; i++ {
+		addr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:"+strconv.Itoa(i))
+		if err != nil {
+			continue
+		}
+		sock, err := net.ListenTCP("tcp4", addr)
+		if err != nil {
+			println(err.Error())
+			continue
+		}
+		sock.Close()
 		time.Sleep(2 * time.Millisecond)
-	}()
-	return l.Addr().(*net.TCPAddr).Port
+		afterPort = i
+		return afterPort
+	}
+	panic("free TCP port not found")
 }
 
 // GetFreeUDPPort returns a free usable UDP address
 // We need to keep an history of the previous port we
 //  allocated, we do this with this global variable.
-var after = 10000 // fist port that we are trying
 func GetFreeUDPPort() int {
-	for i := after + 1; i < after + 1000; i++ {
-		udpAddr, err := net.ResolveUDPAddr("udp4", "127.0.0.1:0")
+	for i := afterPort + 1; i < afterPort + 1000; i++ {
+		udpAddr, err := net.ResolveUDPAddr("udp4", "127.0.0.1:"+strconv.Itoa(i))
 		if err != nil {
 			continue
 		}
@@ -41,17 +42,10 @@ func GetFreeUDPPort() int {
 		if err != nil {
 			continue
 		}
-		addr := sock.LocalAddr().String()
-		_, port, err := net.SplitHostPort(addr)
-		if err != nil {
-			continue
-		}
-		portI, err := strconv.Atoi(port)
-		if err != nil {
-			continue
-		}
-		defer func() { sock.Close(); time.Sleep(2 * time.Millisecond) }()
-		return portI
+		sock.Close()
+		time.Sleep(2 * time.Millisecond)
+		afterPort = i
+		return afterPort
 	}
-	panic("not found")
+	panic("free UDP port not found")
 }
