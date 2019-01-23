e39c70798a907735127519eea2f5b9611bf2ff82
nikkolasg <nicolas.gailly@consensys.net>
2019-01-23 16:37:19 +0100

    libp2p bytes measurement

--- simul/p2p/libp2p/node.go
+++ simul/p2p/libp2p/node.go
@@ -18,6 +18,7 @@ import (
 	"github.com/ConsenSys/handel/simul/lib"
 	libp2p "github.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-host"
+	metrics "github.com/libp2p/go-libp2p-metrics"
 	p2pnet "github.com/libp2p/go-libp2p-net"
 	peer "github.com/libp2p/go-libp2p-peer"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
@@ -82,6 +83,7 @@ type P2PNode struct {
 	setupDoneCh  chan bool
 	setupDone    bool
 	threshold    int
+	reporter     *proxyReporter
 }
 
 // NewP2PNode transforms a lib.Node to a p2p node.
@@ -97,11 +99,13 @@ func NewP2PNode(ctx context.Context, handelNode *lib.Node, n NewRouter, reg P2PR
 	if err != nil {
 		return nil, err
 	}
+	reporter := newProxyReporter()
 	opts := []libp2p.Option{
 		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/%s/tcp/%s", ip, port)),
 		libp2p.DisableRelay(),
 		libp2p.Identity(priv),
 		libp2p.NoSecurity,
+		libp2p.BandwidthReporter(reporter),
 	}
 	basicHost, err := libp2p.New(ctx, opts...)
 	if err != nil {
@@ -149,6 +153,7 @@ func NewP2PNode(ctx context.Context, handelNode *lib.Node, n NewRouter, reg P2PR
 		resendPeriod: 500 * time.Millisecond,
 		setupDoneCh:  make(chan bool, 1),
 		threshold:    threshold,
+		reporter:     reporter,
 	}
 	node.setup.Set(int(node.id.ID()), true)
 	go node.readNexts()
@@ -252,7 +257,7 @@ func (p *P2PNode) incomingSetup(packet *handel.Packet) {
 // Values implements the monitor.Counter interface
 // TODO
 func (p *P2PNode) Values() map[string]float64 {
-	return map[string]float64{}
+	return p.reporter.Values()
 }
 
 func (p *P2PNode) ping(p2 *P2PIdentity) error {
@@ -341,4 +346,33 @@ func getRouter(opts map[string]string) NewRouter {
 		n = pubsub.NewGossipSub
 	}
 	return n
+}
+
+type proxyReporter struct {
+	metrics.Reporter
+	bytesSent int64
+	bytesRcvd int64
+}
+
+func newProxyReporter() *proxyReporter {
+	return &proxyReporter{Reporter: metrics.NewBandwidthCounter()}
+}
+
+func (p *proxyReporter) LogRecvMessage(i int64) {
+	p.bytesRcvd += i
+	p.Reporter.LogRecvMessage(i)
+}
+
+func (p *proxyReporter) LogSentMessage(i int64) {
+	p.bytesSent += i
+	p.Reporter.LogRecvMessage(i)
+}
+
+// Values implement the monitor.Counter interface
+func (p *proxyReporter) Values() map[string]float64 {
+	return map[string]float64{
+		// XXX round up  here might be problem
+		"sentBytes": float64(p.bytesSent),
+		"rcvdBytes": float64(p.bytesRcvd),
+	}
 }
