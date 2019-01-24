8b9edaf85ca490181aa067857d951ddd807ab82b
nikkolasg <nicolas.gailly@consensys.net>
2019-01-24 15:36:18 +0100

    some scripts..

---
+++ simul/plots/thresholds.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+## This script generates the graph that compares handel signature
+## generation time with different thresholds
+##
+import sys
+from lib import *
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+column = "sigen_wall_avg"
+
+files = sys.argv[1:]
+datas = read_datafiles()
+
+for f,v in datas.items():
+    x = v["totalNbOfNodes"]
+    y = v[column].map(lambda x: x * 1000)
+    print("file %s -> %d data points on sigen_wall_avg" % (f,len(y)))
+    label = input("Label for file %s: " % f)
+    if label == "":
+        label = f
+
+    plot(x,y,"-",label,allColors.popleft())
+
+plt.legend(fontsize=fs_label)
+plt.ylabel("signature generation (ms)",fontsize=fs_label)
+plt.xlabel("nodes",fontsize=fs_label)
+# plt.yscale('log')
+plt.show()
